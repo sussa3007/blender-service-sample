4916230156b2ece4e1cec03ff94bb32d434f964c
sussa3007 <sussa1933@gmail.com>
2023-03-08 13:45:15 +0900

    chore: Path 수정

--- src/main/java/com/sample/coresample/constant/path/RenderPath.java
+++ src/main/java/com/sample/coresample/constant/path/RenderPath.java
@@ -3,14 +3,14 @@ package com.sample.coresample.constant.path;
 import com.sample.coresample.constant.blendfile.BlendFileName;
 
 public enum RenderPath {
-//    BLENDER_PATH("C:\\Program Files\\Blender Foundation\\Blender 3.4\\blender.exe"),
-    BLENDER_PATH("D:\\Blender\\blender.exe"),
-//    OUTPUT_PATH("C:\\\\blender\\\\");
-    OUTPUT_PATH("D:\\\\BlenderOutput\\\\"),
-//    SCRIPT_PATH("C:\\blender\\script.py");
-    SCRIPT_PATH("D:\\BlenderOutput\\script.py"),
-//    BLEND_PATH("C:\\blender\\Building_node\\KPB-BrickHouseElements-modified_v02.blend");
-    BLEND_PATH("D:\\BlenderOutput\\Building_node\\"+ BlendFileName.BLEND_HOUSE.getPath());
+    BLENDER_PATH("C:\\Program Files\\Blender Foundation\\Blender 3.4\\blender.exe"),
+//    BLENDER_PATH("D:\\Blender\\blender.exe"),
+    OUTPUT_PATH("C:\\\\blender\\\\"),
+//    OUTPUT_PATH("D:\\\\BlenderOutput\\\\"),
+    SCRIPT_PATH("C:\\blender\\script.py"),
+//    SCRIPT_PATH("D:\\BlenderOutput\\script.py"),
+    BLEND_PATH("C:\\blender\\Building_node\\"+ BlendFileName.BLEND_HOUSE.getPath());
+//    BLEND_PATH("D:\\BlenderOutput\\Building_node\\"+ BlendFileName.BLEND_HOUSE.getPath());
 
     final String path;
     RenderPath(String path) {
