eb08f0501b02cad6c1d0957b343909f6a91f2b2b
sussa3007 <sussa1933@gmail.com>
2023-03-15 13:44:56 +0900

    chore: Generate File 디렉토리 변경

--- src/main/java/com/sample/coresample/constant/path/RenderPath.java
+++ src/main/java/com/sample/coresample/constant/path/RenderPath.java
@@ -5,11 +5,11 @@ import com.sample.coresample.constant.blendfile.BlendFileName;
 public enum RenderPath {
     BLENDER_PATH("C:\\Program Files\\Blender Foundation\\Blender 3.4\\blender.exe"),
 //    BLENDER_PATH("D:\\Blender\\blender.exe"),
-    OUTPUT_PATH("C:\\\\blender\\\\"),
+    OUTPUT_PATH("C:\\\\blender\\\\generate\\\\"),
 //    OUTPUT_PATH("D:\\\\BlenderOutput\\\\"),
     SCRIPT_PATH("C:\\blender\\script.py"),
 //    SCRIPT_PATH("D:\\BlenderOutput\\script.py"),
-    BLEND_PATH("C:\\blender\\Building_node\\"+ BlendFileName.BLEND_HOUSE.getPath());
+    BLEND_PATH("C:\\blender\\Building_node\\"+ BlendFileName.BLEND_HOUSE.getName());
 //    BLEND_PATH("D:\\BlenderOutput\\Building_node\\"+ BlendFileName.BLEND_HOUSE.getPath());
 
     final String path;
